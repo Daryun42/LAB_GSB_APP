d93e371e799ca814ea1f7565f4b60c2ee93a7092
Yanis <anis4221@gmail.com>
2019-06-03 10:17:46 +0200

    Modification classe AccesLocal

--- app/src/main/java/com/example/lab_gsb/AccesLocal.java
+++ app/src/main/java/com/example/lab_gsb/AccesLocal.java
@@ -1,4 +1,14 @@
 package com.example.lab_gsb;
 
+import android.content.Context;
+
 public class AccesLocal {
+
+    private String nomBase = "bdLabGSB.sqlite";
+    private Integer versionBase = 1;
+    private MySQLiteOpenHelper accessBD;
+
+    public AccesLocal(Context contexte){
+        accessBD = new MySQLiteOpenHelper(contexte, nomBase, null, versionBase);
+    }
 }
--- app/src/main/java/com/example/lab_gsb/MySQLiteOpenHelper.java
+++ app/src/main/java/com/example/lab_gsb/MySQLiteOpenHelper.java
@@ -5,11 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
-    private String creation="create table user("
-            + "id TEXT PRIMARY KEY,"
-            + "nom STRING NOT NULL,"
-            + "prenom STRING NOT NULL,"
-            + "email STRING NOT NULL);";
+    private String creation="create table users("
+            + "id INTEGER PRIMARY KEY,"
+            + "nom TEXT NOT NULL,"
+            + "prenom TEXT NOT NULL,"
+            + "email TEXT NOT NULL);";
 
 
     public MySQLiteOpenHelper(@androidx.annotation.Nullable Context context, @androidx.annotation.Nullable String name, @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory, int version) {
@@ -18,7 +18,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(creation);
     }
 
     @Override
